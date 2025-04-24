86b47cf02c7238924be17973b51ee54b7c8f8ef5
bangme4 <mireedui41@gmail.com>
2025-04-24 11:09:41 +0800

    Iredui

---
+++ app/egoreasteregg/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-[url(https://i.pinimg.com/736x/42/f2/2c/42f22c5e05bd514ae0e24dc545261933.jpg)] bg-center bg-cover  flex justify-center items-center">
+        <div className="w-1/2 h-1/2 text-white flex items-center justify-center text-7xl font-semibold bg-black/50 rounded-2xl">
+          welcome Egor culters
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default page;
